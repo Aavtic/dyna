cd4566a88fc5db4192a01709da29f9cec57fb2ec
Aavtic <aadishmkerala@gmail.com>
2025-05-01 21:39:47 +0530

    Added Examples

--- examples/simple_example.c
+++ examples/simple_example.c
@@ -1,14 +1,28 @@
 #include "../dyna.h"
 
 
-int main(void) {
-    DArray array = da_alloc(3, sizeof(int)); 
-    da_append(&array, ELEMENT(1));
-    da_append(&array, ELEMENT(2));
-    da_append(&array, ELEMENT(3));
-    da_append(&array, ELEMENT(4));
+void print_int(DArray *da_array) {
+    int* array = (int *) da_array->start;
 
-    da_print(&array, INTEGER);
+    for (size_t i=0; i<da_array->length; ++i) {
+        printf("%d, ", array[i]);
+    }
+    printf("\n");
+}
+
+int main(void) {
+    DArray d_array = da_alloc(3, sizeof(double));
+
+    /*for (int i=0; i<101; ++i) {*/
+    /*    da_append(&d_array, ELEMENT('a'));*/
+    /*}*/
+
+    for (int i=0; i<50; ++i) {
+        da_append(&d_array, ELEMENT(1.23));
+    }
+    da_print(&d_array, DOUBLE);
+
+    printf("Array size: %ld\nArray length: %ld\nArray capacity: %ld\n", d_array.size, d_array.length, d_array.capacity);
 
     return 0;
 } 
